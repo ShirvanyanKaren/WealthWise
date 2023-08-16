9b023ada579bdcc03b4758b9458f7885de2ccb60
Michael Mattingly <mikeamatt1@hotmail.com>
2023-08-16 00:12:43 -0400

    Updated seeds for budget

--- seeds/createUsersIncomes.js
+++ seeds/createUsersIncomes.js
@@ -1,21 +1,41 @@
-const { faker } = require('@faker-js/faker');
-const { User, Income } = require("../models");
+const { faker } = require("@faker-js/faker");
+const { User, Income, Budget } = require("../models");
+
+const incomeCategories = [
+  "Alimony/Child Support",
+  "Business Income",
+  "Freelance",
+  "Gifts",
+  "Investment Income",
+  "Rental Income",
+  "Salary",
+  "Side Gig",
+  "Other",
+];
 
 const createUsersIncomes = async () => {
-    const users = await User.findAll();
-  
-    for (const user of users) {
-      const incomes = [...Array(10)].map((income) => ({
-        income_name: faker.commerce.productName(),
+  const users = await User.findAll({
+    include: [{ model: Budget, limit: 2 }],
+  });
+
+  for (const user of users) {
+    for (let i = 0; i < 10; i++) {
+      const index = i < 5 ? 0 : 1;
+      const randomCategoryIndex = Math.floor(
+        Math.random() * incomeCategories.length
+      );
+      const randomCategory = incomeCategories[randomCategoryIndex];
+      await Income.create({
+        income_name: faker.lorem.word(),
         user_income_id: user.id,
+        budget_id: user.budgets[index].id,
         amount: faker.commerce.price(),
-        description: faker.commerce.productDescription(),
-        category: faker.commerce.department(),
+        description: faker.lorem.sentence(),
+        category: randomCategory,
         date: faker.date.past(),
-      }));
-  
-      await Income.bulkCreate(incomes);
-    };
-}
-  
+      });
+    }
+  }
+};
+
 module.exports = createUsersIncomes;
