1df3d191f2f8276e09e81171d6bf0b3d0da94fcf
Michael Mattingly <mikeamatt1@hotmail.com>
2023-08-22 20:34:41 -0400

    fixed label in create Budget, and corrected bug in insights

--- controllers/api/insights.js
+++ controllers/api/insights.js
@@ -1,50 +1,51 @@
 const router = require("express").Router();
 const { Budget } = require("../../models");
+const { useAuth } = require("../../utils/auth");
 
+router.get("/", useAuth, async (req, res) => {
+  try {
+    console.log(req.session.user_id);
+    const monthlyBudgetData = await Budget.findAll({
+      where: {
+        user_budget_id: req.session.user_id,
+      },
+      attributes: ["total_savings", "month"],
+    });
 
-router.get('/', async (req, res) => {
-    try {
-      const monthlyBudgetData = await Budget.findAll({
-        where: {
-          user_budget_id: req.session.user_id,
-        },
-        attributes: [
-          "total_savings",
-          "month"
-        ],
-      });
-  
-      console.log(monthlyBudgetData);
+    console.log(monthlyBudgetData);
 
-      const totalMonthlySavings = monthlyBudgetData.reduce((total, item) => parseFloat(total + item.total_savings), 0);
+    const totalMonthlySavings = monthlyBudgetData.reduce(
+      (total, item) => total + item.total_savings,
+      0
+    );
 
-      console.log(totalMonthlySavings);
-  
-    const monthSavingsPerc = monthlyBudgetData.map(item => ({
-        month: item.month,
-        savings_percent: (parseFloat((item.total_savings/(Math.abs(totalMonthlySavings))) * 100).toFixed(2) + "%"),
+    console.log(totalMonthlySavings);
+
+    const monthSavingsPerc = monthlyBudgetData.map((item) => ({
+      month: item.month,
+      savings_percent:
+        parseFloat(
+          (item.total_savings / Math.abs(totalMonthlySavings)) * 100
+        ).toFixed(2) + "%",
     }));
-    
 
-    const averageMonthlySavings = parseFloat(totalMonthlySavings/monthlyBudgetData.length).toFixed(2);
+    const averageMonthlySavings = parseFloat(
+      totalMonthlySavings / monthlyBudgetData.length
+    ).toFixed(2);
 
     console.log(monthSavingsPerc);
 
     const budgetData = {
       totalMonthlySavings,
       averageMonthlySavings,
-      monthSavingsPerc
-    }
+      monthSavingsPerc,
+    };
 
     res.json(budgetData);
-
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'An error occurred while fetching data.' });
-    }
-  });
- 
-  
-
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "An error occurred while fetching data." });
+  }
+});
 
 module.exports = router;
